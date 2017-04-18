060e386c357bbfd4741b519e3b508f813ab1e790
ABackerNINI <1105908317@qq.com>
2017-04-18 22:25:19 +0800

    save

--- FTP_Server/main.cpp
+++ FTP_Server/main.cpp
@@ -1,8 +1,18 @@
 #include <stdio.h>
 #include <iostream>
+#include <conio.h>
+#include "../Resource/Utility/Network/Network.h"
 
 
 int main() {
+	network::Server server;
+
+	network::config_server config;
+	config.port = 80;
+	config.max_connect = 10;
+	server.start(&config, NULL);
+
+	getch();
 
 	return 0;
 }
