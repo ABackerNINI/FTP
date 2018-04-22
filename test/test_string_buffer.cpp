4d78ee7667d8cb185923dd114353541c434c6895
ABackerNINI <1105908317@qq.com>
2018-04-22 22:35:09 +0800

    [add] add long cmd test

--- test/test_string_buffer.cpp
+++ test/test_string_buffer.cpp
@@ -5,7 +5,7 @@
 
 #pragma warning(disable:4996)
 
-void test_string_buffer() {
+void test_normal() {
     ftp_cmd_buffer sb;
     char *s1 = new char[100];
     char *s2 = new char[100];
@@ -26,10 +26,42 @@ void test_string_buffer() {
     sb.push(s3, strlen(s3));
 
     printf("%s\n", sb.pop());
+
+    delete[] s1;
+    delete[] s2;
+    delete[] s3;
+    delete[] s4;
+}
+
+void test_long_cmd() {
+    ftp_cmd_buffer sb;
+    char *s1 = new char[10000];
+    char *s2 = new char[100];
+
+    for (int i = 0; i < 10000; ++i) {
+        s1[i] = '0';
+    }
+    s1[9999] = '\0';
+
+    strcpy(s2, "Supply a user password: PASS password\r\n");
+
+    sb.push(s1, strlen(s1));
+
+    printf("%s\n", sb.pop());
+
+    printf("%s\n", sb.pop());
+    
+    sb.push(s2, strlen(s2));
+
+    printf("%s\n", sb.pop());
+
+    delete[] s1;
 }
 
 int main() {
-    test_string_buffer();
+    test_normal();
+
+    test_long_cmd();
 
     _getch();
 
