5bd46edacb59d8ed0d11b1ace23dfaeda0f35577
ABackerNINI <1105908317@qq.com>
2018-04-22 14:47:48 +0800

    format codes,rename members,add TODO string len check

--- utility/string_buffer/string_buffer.cpp
+++ utility/string_buffer/string_buffer.cpp
@@ -3,10 +3,10 @@
 #include <malloc.h>
 
 string_buffer::string_buffer() :
-    m_Buffer((char *)malloc(STRING_BUFFER_DEFAULT_BUFFER_LEN)),
-    m_Pos(0),
-    m_Size(0),
-    m_Capacity(STRING_BUFFER_DEFAULT_BUFFER_LEN) {
+    m_buffer((char *)malloc(STRING_BUFFER_DEFAULT_BUFFER_LEN)),
+    m_pos(0),
+    m_size(0),
+    m_capacity(STRING_BUFFER_DEFAULT_BUFFER_LEN) {
 }
 
 void string_buffer::push(const char *str, size_t count) {
@@ -16,15 +16,15 @@ void string_buffer::push(const char *str, size_t count) {
 }
 
 char *string_buffer::pop() {
-    char *p0 = m_Buffer + m_Pos;
+    char *p0 = m_buffer + m_pos;
     char *p1 = p0;
-    char *p2 = p1 + m_Size;
+    char *p2 = p1 + m_size;
     for (; p1 != p2; ++p1) {
         if (*p1 == '\r'&&*(p1 + 1) == '\n') {
             *p1 = '\0';
 
-            m_Pos += (p1 - p0) + 2;
-            m_Size = (p2 - p1) - 2;
+            m_pos += (p1 - p0) + 2;
+            m_size = (p2 - p1) - 2;
 
             return p0;
         }
@@ -34,26 +34,27 @@ char *string_buffer::pop() {
 }
 
 size_t string_buffer::size() {
-    return m_Size;
+    return m_size;
 }
 
 string_buffer::~string_buffer() {
-    if (m_Buffer)free(m_Buffer);
+    if (m_buffer)free(m_buffer);
 }
 
 void string_buffer::_buffer(const char *str, size_t count) {
-    if (m_Capacity < m_Size + count) {
-        m_Capacity = m_Size + count + STRING_BUFFER_DEFAULT_BUFFER_LEN;
-        m_Buffer = (char *)realloc(m_Buffer, m_Capacity);
-        //TODO if(m_Buffer==NULL)
+    if (m_capacity < m_size + count) {
+        //TODO string len check
+        m_capacity = m_size + count + STRING_BUFFER_DEFAULT_BUFFER_LEN;
+        m_buffer = (char *)realloc(m_buffer, m_capacity);
+        //TODO if(m_buffer==NULL)
     }
 
-    if (m_Capacity < m_Pos + m_Size + count) {
-        memcpy(m_Buffer, m_Buffer + m_Pos, m_Size);
-        m_Pos = 0;
+    if (m_capacity < m_pos + m_size + count) {
+        memcpy(m_buffer, m_buffer + m_pos, m_size);
+        m_pos = 0;
     }
 
-    memcpy(m_Buffer + m_Pos + m_Size, str, count);
+    memcpy(m_buffer + m_pos + m_size, str, count);
 
-    m_Size += count;
+    m_size += count;
 }
--- utility/string_buffer/string_buffer.h
+++ utility/string_buffer/string_buffer.h
@@ -10,17 +10,23 @@
 class string_buffer {
 public:
     string_buffer();
+
     void push(const char *str, size_t count);
+
     char *pop();
+
     size_t size();
+
     ~string_buffer();
+
 private:
     void _buffer(const char *str, size_t count);
+
 private:
-    char *m_Buffer;
-    size_t m_Pos;
-    size_t m_Size;
-    size_t m_Capacity;
+    char *  m_buffer;
+    size_t  m_pos;
+    size_t  m_size;
+    size_t  m_capacity;
 };
 
-#endif // !_NINI_STRING_BUFFER_H_
+#endif //_NINI_STRING_BUFFER_H_
