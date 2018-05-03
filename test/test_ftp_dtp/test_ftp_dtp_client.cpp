4a8cca7da34bd7ca902f625dc34cca92306085de
ABackerNINI <1105908317@qq.com>
2018-05-03 16:58:08 +0800

    [mod] using modifies of ftp dtp

--- test/test_ftp_dtp/test_ftp_dtp_client.cpp
+++ test/test_ftp_dtp/test_ftp_dtp_client.cpp
@@ -4,27 +4,27 @@
 const char *addr = "192.168.1.107";
 unsigned int port = 20;
 
-void test_ftp_dtp_v1() {
-    ftp_dtp::ftp_dtp dtp;
-
-    dtp.set_fpath("file_to_be_sent");
-    dtp.set_addr(addr);
-    dtp.set_port(port);
-    dtp.start();
-
-    _getch();
-
-    dtp.abort();
-
-    _getch();
-
-    dtp.close();
-}
+//void test_ftp_dtp_v1() {
+//    ftp_dtp::ftp_dtp dtp;
+//
+//    dtp.set_fpath("file_to_be_sent");
+//    dtp.set_addr(addr);
+//    dtp.set_port(port);
+//    dtp.start();
+//
+//    _getch();
+//
+//    dtp.abort();
+//
+//    _getch();
+//
+//    dtp.close();
+//}
 
 void test_ftp_dtp_v2() {
-    ftp_dtp::ftp_dtp_2 dtp;
-    dtp.start(FTP_DTP_TYPE_ACTIVE_RECV, "client_root/file_recved", addr, port);
-    //dtp.start(FTP_DTP_TYPE_ACTIVE_SEND, "client_root/file_to_be_sent", addr, port);
+    ftp_dtp::FtpDtp dtp;
+    //dtp.start(FTP_DTP_TYPE_ACTIVE_RECV, "client_root/file_recved", addr, port);
+    dtp.start(FTP_DTP_TYPE_ACTIVE_SEND, "client_root/file_to_be_sent", addr, port);
 
     _getch();
 
--- test/test_ftp_dtp/test_ftp_dtp_server.cpp
+++ test/test_ftp_dtp/test_ftp_dtp_server.cpp
@@ -3,27 +3,27 @@
 
 unsigned int local_port = 20;
 
-void test_ftp_dtp_v1() {
-    ftp_dtp::ftp_dtp dtp;
-
-    dtp.set_fpath("file_recvd");
-    dtp.set_port(20);
-    dtp.set_passive(true);
-    dtp.start();
-
-    _getch();
-
-    dtp.abort();
-
-    _getch();
-
-    dtp.close();
-}
+//void test_ftp_dtp_v1() {
+//    ftp_dtp::ftp_dtp dtp;
+//
+//    dtp.set_fpath("file_recvd");
+//    dtp.set_port(20);
+//    dtp.set_passive(true);
+//    dtp.start();
+//
+//    _getch();
+//
+//    dtp.abort();
+//
+//    _getch();
+//
+//    dtp.close();
+//}
 
 void test_ftp_dtp_v2() {
-    ftp_dtp::ftp_dtp_2 dtp;
-    dtp.start(FTP_DTP_TYPE_PASSIVE_SEND, "server_root/file_to_be_sent", NULL, 0, &local_port);
-    //dtp.start(FTP_DTP_TYPE_PASSIVE_RECV, "server_root/file_recved", NULL, 0, &local_port);
+    ftp_dtp::FtpDtp dtp;
+    //dtp.start(FTP_DTP_TYPE_PASSIVE_SEND, "server_root/file_to_be_sent", NULL, 0, &local_port);
+    dtp.start(FTP_DTP_TYPE_PASSIVE_RECV, "server_root/file_recved", NULL, 0, &local_port);
 
     _getch();
 
