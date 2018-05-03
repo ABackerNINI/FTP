17267aa32516f14d32b7a99285418a85efb0cfe4
ABackerNINI <1105908317@qq.com>
2018-05-03 14:58:24 +0800

    [sav] working on new ftp_dtp

--- protocol/agent/ftp_dtp/ftp_dtp.cpp
+++ protocol/agent/ftp_dtp/ftp_dtp.cpp
@@ -1,4 +1,5 @@
 #include "ftp_dtp.h"
+#include <assert.h>
 
 /*
  *  ftp_dtp_passive
@@ -56,7 +57,7 @@ void ftp_dtp::ftp_dtp_active::_on_connected(network::CLT_SOCKET_CONTEXT *sock_ct
         char buffer[DEFAULT_BUFFER_LEN];
         size_t count;
 
-        if (m_file.open(m_fpath,"rb") == NULL) {
+        if (m_file.open(m_fpath, "rb") == NULL) {
             return;
         }
 
@@ -179,7 +180,7 @@ void ftp_dtp::ftp_dtp_passive::_on_accepted(network::SVR_SOCKET_CONTEXT *sock_ct
         m_file.close();
 
         //close the server step by step
-        close_connection(sockid);      
+        close_connection(sockid);
         close_listen();
         notify_worker_threads_to_exit();
     } else {
@@ -243,14 +244,14 @@ bool ftp_dtp::ftp_dtp::start() {
         m_dtp_passive->set_sockid_2(m_sockid_2);
         m_dtp_passive->set_is_to_send(m_is_to_send);
         m_dtp_passive->set_fpath(m_fpath);
-        m_dtp_passive->start_listen(m_port);
+        m_dtp_passive->start_listen(&m_port);
     } else {
         m_dtp_active = new ftp_dtp_active();
         m_dtp_active->set_completion_port_2(m_completion_port_2);
         m_dtp_active->set_sockid_2(m_sockid_2);
         m_dtp_active->set_is_to_send(m_is_to_send);
         m_dtp_active->set_fpath(m_fpath);
-        m_dtp_active->connect(m_addr, m_port);
+        m_dtp_active->connect(m_addr,m_port);
     }
 
     return true;
@@ -336,3 +337,358 @@ ftp_dtp::ftp_dtp::~ftp_dtp() {
 //enum ftp_dtp::DATA_TYPE ftp_dtp::ftp_dtp::get_data_type() {
 //    return DATA_TYPE(0);
 //}
+
+/*
+ *  ftp_dtp_passive_send
+ */
+ftp_dtp::ftp_dtp_passive_send::ftp_dtp_passive_send(const char *fpath, unsigned int *local_port, HANDLE completion_port_2, SOCKET sockid_2) :
+    m_completion_port_2(completion_port_2),
+    m_sockid_2(sockid_2) {
+    if (m_file.open(fpath, "rb") != NULL) {
+        network::Server::start_listen(local_port);
+    }
+}
+
+bool ftp_dtp::ftp_dtp_passive_send::abort() {
+    network::Server::close();
+
+    return true;
+}
+
+void ftp_dtp::ftp_dtp_passive_send::event_handler(network::SVR_SOCKET_CONTEXT *sock_ctx, int ev) {
+    switch (ev) {
+    case EVENT_CONNECTED:
+        _on_connected(sock_ctx);
+        break;
+    case EVENT_RECEIVED:
+        _on_recvd(sock_ctx);
+        break;
+    case EVENT_SENT:
+        _on_sent(sock_ctx);
+        break;
+    case EVENT_CLOSED:
+        _on_closed(sock_ctx);
+        break;
+    default:
+        break;
+    }
+}
+
+void ftp_dtp::ftp_dtp_passive_send::_on_connected(network::SVR_SOCKET_CONTEXT *sock_ctx) {
+    char buffer[DEFAULT_BUFFER_LEN];
+    size_t count;
+    SOCKET sockid = sock_ctx->m_client_sockid;
+
+    //m_fsize = m_file.size();
+
+    while (!m_file.feof()) {
+        count = m_file.read(buffer, sizeof(char), DEFAULT_BUFFER_LEN);
+
+        if (m_file.ferror() || !send(sockid, buffer, count)) {
+            break;
+        }
+        m_bytes_transfered += count;
+    }
+
+    m_file.close();
+
+    //close the server step by step
+    close_connection(sockid);
+    close_listen();
+    notify_worker_threads_to_exit();
+}
+
+void ftp_dtp::ftp_dtp_passive_send::_on_sent(network::SVR_SOCKET_CONTEXT *sock_ctx) {
+}
+
+void ftp_dtp::ftp_dtp_passive_send::_on_recvd(network::SVR_SOCKET_CONTEXT *sock_ctx) {
+}
+
+void ftp_dtp::ftp_dtp_passive_send::_on_closed(network::SVR_SOCKET_CONTEXT *sock_ctx) {
+    m_file.close();
+
+    //close the server step by step
+    close_connection(sock_ctx->m_client_sockid);
+    close_listen();
+    notify_worker_threads_to_exit();
+
+    //post done-msg back
+    if (m_completion_port_2) {
+        PostQueuedCompletionStatus(m_completion_port_2, EVENT_USER_FIRST, (ULONG_PTR)&m_sockid_2, NULL);
+    }
+
+    printf("done\n");
+}
+
+/*
+ *  ftp_dtp_passive_recv
+*/
+ftp_dtp::ftp_dtp_passive_recv::ftp_dtp_passive_recv(const char *fpath, unsigned int *local_port, HANDLE completion_port_2, SOCKET sockid_2) :
+    m_completion_port_2(completion_port_2),
+    m_sockid_2(sockid_2) {
+    if (m_file.open(fpath, "wb")) {
+        network::Server::start_listen(0);
+    }
+}
+
+bool ftp_dtp::ftp_dtp_passive_recv::abort() {
+    network::Server::close();
+
+    return true;
+}
+
+void ftp_dtp::ftp_dtp_passive_recv::event_handler(network::SVR_SOCKET_CONTEXT *sock_ctx, int ev) {
+    switch (ev) {
+    case EVENT_CONNECTED:
+        _on_connected(sock_ctx);
+        break;
+    case EVENT_RECEIVED:
+        _on_recvd(sock_ctx);
+        break;
+    case EVENT_SENT:
+        _on_sent(sock_ctx);
+        break;
+    case EVENT_CLOSED:
+        _on_closed(sock_ctx);
+        break;
+    default:
+        break;
+    }
+}
+
+void ftp_dtp::ftp_dtp_passive_recv::_on_connected(network::SVR_SOCKET_CONTEXT *sock_ctx) {
+}
+
+void ftp_dtp::ftp_dtp_passive_recv::_on_sent(network::SVR_SOCKET_CONTEXT *sock_ctx) {
+}
+
+void ftp_dtp::ftp_dtp_passive_recv::_on_recvd(network::SVR_SOCKET_CONTEXT *sock_ctx) {
+    m_file.write(sock_ctx->m_buffer, sizeof(char), sock_ctx->m_bytes_transferred);
+    m_bytes_transfered += sock_ctx->m_bytes_transferred;
+}
+
+void ftp_dtp::ftp_dtp_passive_recv::_on_closed(network::SVR_SOCKET_CONTEXT *sock_ctx) {
+    m_file.close();
+
+    //close the server step by step
+    close_connection(sock_ctx->m_client_sockid);
+    close_listen();
+    notify_worker_threads_to_exit();
+
+    //post done-msg back
+    if (m_completion_port_2) {
+        PostQueuedCompletionStatus(m_completion_port_2, EVENT_USER_FIRST, (ULONG_PTR)&m_sockid_2, NULL);
+    }
+
+    printf("done\n");
+}
+
+/*
+ *  ftp_dtp_active_send
+ */
+ftp_dtp::ftp_dtp_active_send::ftp_dtp_active_send(const char *fpath, const char *addr, unsigned int port, HANDLE completion_port_2, SOCKET sockid_2) :
+    m_completion_port_2(completion_port_2),
+    m_sockid_2(sockid_2) {
+    if (m_file.open(fpath, "rb")) {
+        network::Client::connect(addr, port);
+    }
+}
+
+bool ftp_dtp::ftp_dtp_active_send::abort() {
+    network::Client::close();
+
+    return true;
+}
+
+void ftp_dtp::ftp_dtp_active_send::event_handler(network::CLT_SOCKET_CONTEXT *sock_ctx, int ev) {
+    switch (ev) {
+    case EVENT_CONNECTED:
+        _on_connected(sock_ctx);
+        break;
+    case EVENT_RECEIVED:
+        _on_recvd(sock_ctx);
+        break;
+    case EVENT_SENT:
+        _on_sent(sock_ctx);
+        break;
+    case EVENT_CLOSED:
+        _on_closed(sock_ctx);
+        break;
+    default:
+        break;
+    }
+}
+
+void ftp_dtp::ftp_dtp_active_send::_on_connected(network::CLT_SOCKET_CONTEXT *sock_ctx) {
+    char buffer[DEFAULT_BUFFER_LEN];
+    size_t count;
+
+    //m_fsize = m_file.size();
+
+    while (!m_file.feof()) {
+        count = m_file.read(buffer, sizeof(char), DEFAULT_BUFFER_LEN);
+
+        if (m_file.ferror() || !send(buffer, count)) {
+            break;
+        }
+        m_bytes_transfered += count;
+    }
+
+    m_file.close();
+
+    //close the client step by step
+    close_connection();
+    notify_worker_threads_to_exit();
+}
+
+void ftp_dtp::ftp_dtp_active_send::_on_sent(network::CLT_SOCKET_CONTEXT *sock_ctx) {
+}
+
+void ftp_dtp::ftp_dtp_active_send::_on_recvd(network::CLT_SOCKET_CONTEXT *sock_ctx) {
+}
+
+void ftp_dtp::ftp_dtp_active_send::_on_closed(network::CLT_SOCKET_CONTEXT *sock_ctx) {
+    m_file.close();
+
+    //close the client step by step
+    close_connection();
+    notify_worker_threads_to_exit();
+
+    //post done-msg back
+    if (m_completion_port_2) {
+        PostQueuedCompletionStatus(m_completion_port_2, EVENT_USER_FIRST, (ULONG_PTR)&m_sockid_2, NULL);
+    }
+
+    printf("done\n");
+}
+
+/*
+ *  ftp_dtp_active_recv
+ */
+ftp_dtp::ftp_dtp_active_recv::ftp_dtp_active_recv(const char *fpath, const char *addr, unsigned int port, HANDLE completion_port_2, SOCKET sockid_2) :
+    m_completion_port_2(completion_port_2),
+    m_sockid_2(sockid_2) {
+    if (m_file.open(fpath, "wb")) {
+        network::Client::connect(addr, port);
+    }
+}
+
+bool ftp_dtp::ftp_dtp_active_recv::abort() {
+    network::Client::close();
+
+    return true;
+}
+
+void ftp_dtp::ftp_dtp_active_recv::event_handler(network::CLT_SOCKET_CONTEXT *sock_ctx, int ev) {
+    switch (ev) {
+    case EVENT_CONNECTED:
+        _on_connected(sock_ctx);
+        break;
+    case EVENT_RECEIVED:
+        _on_recvd(sock_ctx);
+        break;
+    case EVENT_SENT:
+        _on_sent(sock_ctx);
+        break;
+    case EVENT_CLOSED:
+        _on_closed(sock_ctx);
+        break;
+    default:
+        break;
+    }
+}
+
+void ftp_dtp::ftp_dtp_active_recv::_on_connected(network::CLT_SOCKET_CONTEXT *sock_ctx) {
+}
+
+void ftp_dtp::ftp_dtp_active_recv::_on_sent(network::CLT_SOCKET_CONTEXT *sock_ctx) {
+}
+
+void ftp_dtp::ftp_dtp_active_recv::_on_recvd(network::CLT_SOCKET_CONTEXT *sock_ctx) {
+    m_file.write(sock_ctx->m_buffer, sizeof(char), sock_ctx->m_bytes_transferred);
+    m_bytes_transfered += sock_ctx->m_bytes_transferred;
+}
+
+void ftp_dtp::ftp_dtp_active_recv::_on_closed(network::CLT_SOCKET_CONTEXT *sock_ctx) {
+    m_file.close();
+
+    //close the client step by step
+    close_connection();
+    notify_worker_threads_to_exit();
+
+    //post done-msg back
+    if (m_completion_port_2) {
+        PostQueuedCompletionStatus(m_completion_port_2, EVENT_USER_FIRST, (ULONG_PTR)&m_sockid_2, NULL);
+    }
+
+    printf("done\n");
+}
+
+/*
+ *  ftp_dtp
+ */
+ftp_dtp::ftp_dtp_2::ftp_dtp_2() :
+    m_dtp(NULL),
+    m_dtp_type(FTP_DTP_TYPE_NULL) {
+}
+
+bool ftp_dtp::ftp_dtp_2::start(int dtp_type, const char *fpath, const char *addr, unsigned int port, unsigned int *local_port /*= NULL*/, HANDLE completion_port_2 /*= NULL*/, SOCKET sockid_2 /*= INVALID_SOCKET*/) {
+    if (!m_dtp) {
+        switch (dtp_type) {
+        case FTP_DTP_TYPE_PASSIVE_SEND:
+            m_dtp = new ftp_dtp_passive_send(fpath, local_port, completion_port_2, sockid_2);
+            break;
+        case FTP_DTP_TYPE_PASSIVE_RECV:
+            m_dtp = new ftp_dtp_passive_recv(fpath, local_port, completion_port_2, sockid_2);
+            break;
+        case FTP_DTP_TYPE_ACTIVE_SEND:
+            m_dtp = new ftp_dtp_active_send(fpath, addr, port, completion_port_2, sockid_2);
+            break;
+        case FTP_DTP_TYPE_ACTIVE_RECV:
+            m_dtp = new ftp_dtp_active_recv(fpath, addr, port, completion_port_2, sockid_2);
+            break;
+        default:
+            assert(false);
+        }
+        m_dtp_type = dtp_type;
+
+        return true;
+    }
+
+    return false;
+}
+
+bool ftp_dtp::ftp_dtp_2::abort() {
+    switch (m_dtp_type) {
+    case FTP_DTP_TYPE_PASSIVE_SEND:
+        ((ftp_dtp_passive_send *)m_dtp)->abort();
+        break;
+    case FTP_DTP_TYPE_PASSIVE_RECV:
+        ((ftp_dtp_passive_recv *)m_dtp)->abort();
+        break;
+    case FTP_DTP_TYPE_ACTIVE_SEND:
+        ((ftp_dtp_active_send *)m_dtp)->abort();
+        break;
+    case FTP_DTP_TYPE_ACTIVE_RECV:
+        ((ftp_dtp_active_recv *)m_dtp)->abort();
+        break;
+    default:
+        assert(false);
+    }
+
+    return true;
+}
+
+bool ftp_dtp::ftp_dtp_2::close() {
+    if (m_dtp) {
+        this->abort();
+        delete m_dtp;
+        m_dtp = NULL;
+        m_dtp_type = FTP_DTP_TYPE_NULL;
+    }
+
+    return true;
+}
+
+ftp_dtp::ftp_dtp_2::~ftp_dtp_2() {
+}
--- protocol/agent/ftp_dtp/ftp_dtp.h
+++ protocol/agent/ftp_dtp/ftp_dtp.h
@@ -6,11 +6,17 @@
 #include "../../../utility/file/file.h"
 #include "../../../utility/network/network.h"
 
-namespace ftp_dtp {
-
 #define DEBUG 1
 #define DEBUG_ERROR_CHECK 1
 #define DEFAULT_BUFFER_LEN 1024
+
+#define FTP_DTP_TYPE_NULL           (-1)
+#define FTP_DTP_TYPE_PASSIVE_SEND   0
+#define FTP_DTP_TYPE_PASSIVE_RECV   1
+#define FTP_DTP_TYPE_ACTIVE_SEND    2
+#define FTP_DTP_TYPE_ACTIVE_RECV    3
+
+namespace ftp_dtp {
 
     enum STATUS {
         STA_NULL,
@@ -101,8 +107,9 @@ namespace ftp_dtp {
         bool                m_is_to_send;
         const char *        m_fpath;
         size_t              m_fsize;
-        size_t              m_bytes_transfered;
         file::File          m_file;
+
+        size_t              m_bytes_transfered;
     };
 
     class ftp_dtp {
@@ -140,10 +147,133 @@ namespace ftp_dtp {
         bool                m_is_to_send;
         const char*         m_fpath;
         const char*         m_addr;
-        int                 m_port;
+        unsigned int        m_port;
 
         ftp_dtp_active*     m_dtp_active;
         ftp_dtp_passive*    m_dtp_passive;
+    };
+
+
+
+    class ftp_dtp_passive_send : public network::Server {
+    public:
+        ftp_dtp_passive_send(const char *fpath, unsigned int *local_port, HANDLE completion_port_2, SOCKET sockid_2);
+
+        bool abort();
+
+    protected:
+        virtual void event_handler(network::SVR_SOCKET_CONTEXT *sock_ctx, int ev) override;
+
+        void _on_connected(network::SVR_SOCKET_CONTEXT *sock_ctx);
+
+        void _on_sent(network::SVR_SOCKET_CONTEXT *sock_ctx);
+
+        void _on_recvd(network::SVR_SOCKET_CONTEXT *sock_ctx);
+
+        void _on_closed(network::SVR_SOCKET_CONTEXT *sock_ctx);
+
+    protected:
+        HANDLE              m_completion_port_2;//post back
+        SOCKET              m_sockid_2;//post back
+
+        file::File          m_file;
+
+        size_t              m_bytes_transfered;
+    };
+
+    class ftp_dtp_passive_recv : public network::Server {
+    public:
+        ftp_dtp_passive_recv(const char *fpath, unsigned int *local_port, HANDLE completion_port_2, SOCKET sockid_2);
+
+        bool abort();
+
+    protected:
+        virtual void event_handler(network::SVR_SOCKET_CONTEXT *sock_ctx, int ev) override;
+
+        void _on_connected(network::SVR_SOCKET_CONTEXT *sock_ctx);
+
+        void _on_sent(network::SVR_SOCKET_CONTEXT *sock_ctx);
+
+        void _on_recvd(network::SVR_SOCKET_CONTEXT *sock_ctx);
+
+        void _on_closed(network::SVR_SOCKET_CONTEXT *sock_ctx);
+
+    protected:
+        HANDLE              m_completion_port_2;//post back
+        SOCKET              m_sockid_2;//post back
+
+        file::File          m_file;
+
+        size_t              m_bytes_transfered;
+    };
+
+    class ftp_dtp_active_send : public network::Client {
+    public:
+        ftp_dtp_active_send(const char *fpath, const char *addr, unsigned int port, HANDLE completion_port_2, SOCKET sockid_2);
+
+        bool abort();
+
+    protected:
+        virtual void event_handler(network::CLT_SOCKET_CONTEXT *sock_ctx, int ev) override;
+
+        void _on_connected(network::CLT_SOCKET_CONTEXT *sock_ctx);
+
+        void _on_sent(network::CLT_SOCKET_CONTEXT *sock_ctx);
+
+        void _on_recvd(network::CLT_SOCKET_CONTEXT *sock_ctx);
+
+        void _on_closed(network::CLT_SOCKET_CONTEXT *sock_ctx);
+
+    protected:
+        HANDLE              m_completion_port_2;//post back
+        SOCKET              m_sockid_2;//post back
+
+        file::File          m_file;
+
+        size_t              m_bytes_transfered;
+    };
+
+    class ftp_dtp_active_recv : public network::Client {
+    public:
+        ftp_dtp_active_recv(const char *fpath, const char *addr, unsigned int port, HANDLE completion_port_2, SOCKET sockid_2);
+
+        bool abort();
+
+    protected:
+        virtual void event_handler(network::CLT_SOCKET_CONTEXT *sock_ctx, int ev) override;
+
+        void _on_connected(network::CLT_SOCKET_CONTEXT *sock_ctx);
+
+        void _on_sent(network::CLT_SOCKET_CONTEXT *sock_ctx);
+
+        void _on_recvd(network::CLT_SOCKET_CONTEXT *sock_ctx);
+
+        void _on_closed(network::CLT_SOCKET_CONTEXT *sock_ctx);
+
+    protected:
+        HANDLE              m_completion_port_2;//post back
+        SOCKET              m_sockid_2;//post back
+
+        file::File          m_file;
+
+        size_t              m_bytes_transfered;
+    };
+
+    class ftp_dtp_2 {
+    public:
+        ftp_dtp_2();
+
+        bool start(int dtp_type, const char *fpath, const char *addr, unsigned int port, unsigned int *local_port = NULL, HANDLE completion_port_2 = NULL, SOCKET sockid_2 = INVALID_SOCKET);
+
+        bool abort();
+
+        bool close();
+
+        ~ftp_dtp_2();
+
+    protected:
+        void *  m_dtp;
+        int     m_dtp_type;
     };
 }
 
